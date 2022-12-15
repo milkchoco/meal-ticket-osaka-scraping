3c9e4437d1d8358ebfb00f5aa22f2cb474d84683
yuzu <s.yuzu441@gmail.com>
2022-12-15 22:28:15 +0900

    fix: form select

--- src/targetData.ts
+++ src/targetData.ts
@@ -1,16 +1,14 @@
 export const area = {
   // It takes so long time to enable this
-  //all: 'area_all',
-  north: 'feas_0_0_0',
-  south: 'feas_0_0_1',
-  osakaCasle: 'feas_0_0_2',
-  abeno: 'feas_0_0_3',
-  bayarea: 'feas_0_0_4',
-  hokusetsu: 'feas_0_0_5',
-  kitakawachi: 'feas_0_0_6',
-  nakakawachi: 'feas_0_0_7',
-  minamikawachi: 'feas_0_0_8',
-  sensyu: 'feas_0_0_9',
+  osakashi: 'area01',
+  sakai: 'area02',
+  toyonaka: 'area03',
+  mishima: 'area04',
+  kitakawachi: 'area05',
+  nakakawachi: 'area06',
+  senboku: 'area07',
+  sennan: 'area08',
+  minamikawachi: 'area09',
 }
 
 export interface Genre {
@@ -19,72 +17,22 @@ export interface Genre {
 }
 
 export const genre: Genre[] = [
-  {
-    id: 'feas_0_1_0',
-    t: '居酒屋',
-  },
-  {
-    id: 'feas_0_1_1',
-    t: '和食',
-  },
-  {
-    id: 'feas_0_1_2',
-    t: '寿司・魚料理',
-  },
-  {
-    id: 'feas_0_1_3',
-    t: 'うどん・そば',
-  },
-  {
-    id: 'feas_0_1_4',
-    t: '鍋',
-  },
-  {
-    id: 'feas_0_1_5',
-    t: 'お好み焼き・たこ焼き',
-  },
-  {
-    id: 'feas_0_1_6',
-    t: 'ラーメン・つけ麺',
-  },
-  {
-    id: 'feas_0_1_7',
-    t: '郷土料理',
-  },
-  {
-    id: 'feas_0_1_8',
-    t: '洋食・西洋料理',
-  },
-  {
-    id: 'feas_0_1_9',
-    t: 'カレー',
-  },
-  {
-    id: 'feas_0_1_10',
-    t: '焼肉・ホルモン',
-  },
-  {
-    id: 'feas_0_1_11',
-    t: 'イタリアン・フレンチ',
-  },
-  {
-    id: 'feas_0_1_12',
-    t: '中華料理',
-  },
-  {
-    id: 'feas_0_1_13',
-    t: 'アジア・エスニック料理',
-  },
-  {
-    id: 'feas_0_1_14',
-    t: 'カフェ・スイーツ',
-  },
-  {
-    id: 'feas_0_1_15',
-    t: 'ホテルレストラン',
-  },
-  {
-    id: 'feas_0_1_16',
-    t: 'その他',
-  },
+  { id: 'category_all', t: 'すべてのジャンル' },
+  // { id: 'feas_0_1_0', t: '居酒屋' },
+  // { id: 'feas_0_1_1', t: '和食' },
+  // { id: 'feas_0_1_2', t: '寿司・魚料理' },
+  // { id: 'feas_0_1_3', t: 'うどん・そば' },
+  // { id: 'feas_0_1_4', t: '鍋' },
+  // { id: 'feas_0_1_5', t: 'お好み焼き・たこ焼き' },
+  // { id: 'feas_0_1_6', t: 'ラーメン・つけ麺' },
+  // { id: 'feas_0_1_7', t: '郷土料理' },
+  // { id: 'feas_0_1_8', t: '洋食・西洋料理' },
+  // { id: 'feas_0_1_9', t: 'カレー' },
+  // { id: 'feas_0_1_10', t: '焼肉・ホルモン' },
+  // { id: 'feas_0_1_11', t: 'イタリアン・フレンチ' },
+  // { id: 'feas_0_1_12', t: '中華料理' },
+  // { id: 'feas_0_1_13', t: 'アジア・エスニック料理' },
+  // { id: 'feas_0_1_14', t: 'カフェ・スイーツ' },
+  // { id: 'feas_0_1_15', t: 'ホテルレストラン' },
+  { id: 'feas_0_1_16', t: 'その他' },
 ]
